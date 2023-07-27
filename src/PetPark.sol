4241b9b9ddd56d902f9e2dec709ec022960dd76f
Kamal Nayan <95926324+kamalbuilds@users.noreply.github.com>
2023-07-27 22:38:42 +0000

    added remappings

--- src/PetPark.sol
+++ src/PetPark.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import {Ownable} from "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import {Ownable} from "openzeppelin-contracts/access/Ownable.sol";
 
 contract PetPark is Ownable {
     enum AnimalType {
